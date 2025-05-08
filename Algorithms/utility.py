4573a0e8b5a1b5fe3a3115cea100b6766ffe5d66
chanho26 <38136331+chanho26@users.noreply.github.com>
2025-05-08 17:30:16 +0900

    Update utility.py

--- Algorithms/utility.py
+++ Algorithms/utility.py
@@ -1,6 +1,10 @@
 import torch
 import numpy as np
 from torchvision import datasets, transforms
+import scipy
+import os
+import random
+import copy
 
 
 def data_next(dataloader, trainloader): # 한 round에 여러 device들이 학습되므로 dataloader가 차례차례 돌아가도록 하는 코드
@@ -127,13 +131,59 @@ def loader(args):
                 traindata_split.append(torch.utils.data.Subset(traindata, index_per_worker[j]))
 
 
-    train_loader = []
-    for i in range(args.num_workers):
-        train_loader.append(torch.utils.data.DataLoader(traindata_split[i], batch_size=int(args.train_batch_size[i]), shuffle=True))          
+    elif args.dataset == 'ImageNet':
 
-    # for i, v in enumerate(train_loader[0]):
-    #     print(len(v))
-    #     break
+        def list_chunk(lst, n):
+            quo = len(lst) // n
+            rem = len(lst) % n
+
+            num_elems = [quo] * n
+            for i in range(rem):
+                num_elems[i] += 1
+
+            num_start = [0]
+            for i in range(n-1):
+                num_start.append(num_start[i] + num_elems[i])
+
+            return [lst[num_start[i]: num_start[i]+num_elems[i]] for i in range(n)]
+
+        traindata_split = [copy.deepcopy([]) for i in range(args.num_workers)]
+        train_folder_dir = './data/ImageNet/train'
+        folders_name = os.listdir(train_folder_dir)
+        for idx, name in enumerate(folders_name[:100]):
+            image_dir = os.path.join(train_folder_dir, name)
+            file_name = os.listdir(image_dir)
+            file_tuple = list(zip(file_name, [idx] * len(file_name)))
+            random.shuffle(file_tuple)
+            split_files = list_chunk(file_tuple, args.num_workers)
+            for j in range(args.num_workers):
+                traindata_split[j] += split_files[j]
+
+        
+        testdata = []       
+        test_folder_dir = './data/ImageNet/val'
+        folders_name = os.listdir(test_folder_dir)
+        for idx, name in enumerate(folders_name[:100]):
+            image_dir = os.path.join(test_folder_dir, name)
+            file_name = os.listdir(image_dir)
+            file_tuple = list(zip(file_name, [idx] * len(file_name)))
+            testdata += file_tuple 
+
+
+        # traindata = datasets.ImageNet(root='./data/ImageNet', split='train', transform=transform_train)
+        # testdata = datasets.ImageNet(root='./data/ImageNet', split='val', transform=transform_test)
+
+        test_loader = torch.utils.data.DataLoader(testdata, batch_size=args.test_batch_size, shuffle=False, pin_memory=False)
+ 
+    train_loader = []
+
+    for i in range(args.num_workers):
+        train_loader.append(torch.utils.data.DataLoader(traindata_split[i], batch_size=int(args.train_batch_size[i]), shuffle=True, pin_memory=False))
+
+    # for idx, data in enumerate(train_loader[0]):
+    #     print(idx, data)
+    #     asdfadsfdsf
+
 
     dataloader = []
     for i in range(args.num_workers):
@@ -142,3 +192,208 @@ def loader(args):
     
     return dataloader, train_loader, test_loader
 
+
+
+def loader_text(args):
+    from sklearn.datasets import fetch_20newsgroups
+    from sklearn.model_selection import train_test_split
+    from sklearn.feature_extraction.text import CountVectorizer
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.preprocessing import LabelEncoder
+
+    # 1. 데이터 준비
+    class NewsGroupDataset(torch.utils.data.Dataset):
+        def __init__(self, texts, labels, vocab):
+            self.texts = []
+            self.labels = []
+            self.vocab = vocab
+
+            # 빈 텍스트 필터링 (데이터셋 내에 비어 있는 (공백) 데이터가 있나봄)
+            for text, label in zip(texts, labels):
+                if text.strip():    # 빈 텍스트가 아닌 경우에만 추가
+                    self.texts.append(text)
+                    self.labels.append(label)
+
+        def __len__(self):
+            return len(self.texts)
+        
+        def __getitem__(self, idx):
+            text = self.texts[idx]
+            label = self.labels[idx]
+            tokenized = [self.vocab.get(word, self.vocab['<unk>']) for word in text.split()]
+            return torch.tensor(tokenized, dtype=torch.long), torch.tensor(label, dtype=torch.long)
+        
+    # 데이터를 가져오고 전처리
+    newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+    texts, labels = newsgroups.data, newsgroups.target
+
+    # 데이터셋 분할
+    train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, test_size=0.2, random_state=42)
+
+    # 단어 빈도를 기반으로 어휘 새성
+    vectorizer = CountVectorizer(max_features=20000, stop_words='english')
+    vectorizer.fit(train_texts)
+    vocab = {word: idx + 1 for idx, word in enumerate(vectorizer.get_feature_names_out())}
+    vocab['<unk>'] = 0
+
+    # 데이터셋 및 데이터로더 생성
+    train_dataset = NewsGroupDataset(train_texts, train_labels, vocab)
+    test_dataset = NewsGroupDataset(test_texts, test_labels, vocab)
+
+    traindata_split = [copy.deepcopy([]) for i in range(args.num_workers)]
+
+    idx_data_per_label = [copy.deepcopy([]) for _ in range(len(newsgroups.target_names))]
+
+    for idx, (_, label) in enumerate(train_dataset):
+        idx_data_per_label[int(label)].append(idx)
+
+    def list_chunk(lst, n):
+        quo = len(lst) // n
+        rem = len(lst) % n
+
+        num_elems = [quo] * n
+        for i in range(rem):
+            num_elems[i] += 1
+
+        num_start = [0]
+        for i in range(n-1):
+            num_start.append(num_start[i] + num_elems[i])
+
+        return [lst[num_start[i]: num_start[i]+num_elems[i]] for i in range(n)]
+
+    for i in range(len(idx_data_per_label)):
+        random.shuffle(idx_data_per_label[i])
+        split_data = list_chunk(idx_data_per_label[i], args.num_workers)
+
+        for j in range(args.num_workers):
+            traindata_split[j] += [train_dataset[idx] for idx in split_data[j]]   
+
+    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=lambda x: collate_batch(x, vocab))
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False, collate_fn=lambda x: clip_sequences(x, vocab))
+
+    def collate_batch(batch, vocab):
+        texts, labels = zip(*batch)
+        lengths = [len(text) for text in texts]
+        padded_texts = torch.nn.utils.rnn.pad_sequence(texts, batch_first=True, padding_value=vocab['<unk>'])
+        return padded_texts, torch.tensor(labels), torch.tensor(lengths)     
+    
+    def clip_sequences(batch, vocab, max_len=512):
+        texts, labels = zip(*batch)
+        texts = [text[:max_len] for text in texts]
+        lengths = [len(text) for text in texts]
+        texts = torch.nn.utils.rnn.pad_sequence([torch.tensor(text) for text in texts], batch_first=True, padding_value=vocab['<unk>'])
+        return texts, torch.tensor(labels), torch.tensor(lengths)
+    
+
+
+    train_loader = []
+    for i in range(args.num_workers):
+        # train_loader.append(torch.utils.data.DataLoader(traindata_split[i], batch_size=int(args.train_batch_size[i]), \
+        #                                                 shuffle=True, collate_fn=lambda x: collate_batch(x, vocab)))
+        train_loader.append(torch.utils.data.DataLoader(traindata_split[i], batch_size=int(args.train_batch_size[i]), \
+                                                        shuffle=True, collate_fn=lambda x: clip_sequences(x, vocab)))
+
+    # for idx, data in enumerate(train_loader[0]):
+    #     print(idx, data)
+    #     asdfadsfdsf
+
+    dataloader = []
+    for i in range(args.num_workers):
+        dataloader += [enumerate(train_loader[i])] 
+
+    
+    return dataloader, train_loader, test_loader, vocab
+
+
+
+def loader_text_AGNEWS(args):
+    from torchtext.datasets import AG_NEWS
+    from torchtext.data.utils import get_tokenizer
+    from torchtext.vocab import build_vocab_from_iterator
+    from torch.nn.utils.rnn import pad_sequence
+
+
+    def yield_tokens(data_iter, tokenizer):
+        for _, text in data_iter:
+            yield tokenizer(text)
+
+    tokenizer = get_tokenizer("basic_english")
+    train_iter, test_iter = AG_NEWS(split=("train", "test"))
+    vocab = build_vocab_from_iterator(yield_tokens(train_iter, tokenizer), specials=["<unk>"])
+    vocab.set_default_index(vocab["<unk>"])
+
+    def text_pipeline(x):
+        return vocab(tokenizer(x))
+    
+    def label_pipeline(x):
+        return int(x) - 1
+    
+    def collate_batch(batch):
+        # text_list, label_list = [], []
+        text_list, label_list, offsets = [], [], [0]
+
+        for label, text in batch:
+            # text_list.append(torch.tensor(text_pipeline(text), dtype=torch.long))
+            # label_list.append(torch.tensor(label_pipeline(label), dtype=torch.long))
+
+            label_list.append(torch.tensor(label_pipeline(label), dtype=torch.long))
+            processed_text = torch.tensor(text_pipeline(text), dtype=torch.long)
+            text_list.append(processed_text)
+            offsets.append(processed_text.size(0))
+
+        # text_list = pad_sequence(text_list, batch_first=True, padding_value=0)
+        # label_list = torch.tensor(label_list, dtype=torch.long)
+
+        label_list = torch.tensor(label_list, dtype=torch.long)
+        offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+        text_list = torch.cat(text_list)
+
+        # return text_list, label_list
+        return text_list, label_list, offsets
+    
+    train_dataset, test_dataset = list(train_iter), list(test_iter)
+
+    traindata_split = [copy.deepcopy([]) for _ in range(args.num_workers)]
+    idx_data_per_label = [copy.deepcopy([]) for _ in range(4)]
+
+    for idx, (label, _) in enumerate(train_dataset):
+        idx_data_per_label[label_pipeline(label)].append(idx)
+
+    def list_chunk(lst, n):
+        quo = len(lst) // n
+        rem = len(lst) % n
+
+        num_elems = [quo] * n
+        for i in range(rem):
+            num_elems[i] += 1
+
+        num_start = [0]
+        for i in range(n-1):
+            num_start.append(num_start[i] + num_elems[i])
+
+        return [lst[num_start[i]: num_start[i]+num_elems[i]] for i in range(n)]
+
+    for i in range(len(idx_data_per_label)):
+        random.shuffle(idx_data_per_label[i])
+        split_data = list_chunk(idx_data_per_label[i], args.num_workers)
+
+        for j in range(args.num_workers):
+            traindata_split[j] += [train_dataset[idx] for idx in split_data[j]]   
+
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False, collate_fn=collate_batch)
+    
+    train_loader = []
+    for i in range(args.num_workers):
+        train_loader.append(torch.utils.data.DataLoader(traindata_split[i], batch_size=int(args.train_batch_size[i]), \
+                                                        shuffle=True, collate_fn=collate_batch))
+
+    # for idx, data in enumerate(train_loader[0]):
+    #     print(idx, data)
+    #     asdfadsfdsf
+
+    dataloader = []
+    for i in range(args.num_workers):
+        dataloader += [enumerate(train_loader[i])] 
+
+    
+    return dataloader, train_loader, test_loader, vocab
